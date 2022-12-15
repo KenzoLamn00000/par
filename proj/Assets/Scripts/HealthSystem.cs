0ba418c4c6d86fa99edad14c2121bf288ff15061
KenzoLamn00000 <Lamn.mattias@maranathastudents.org>
2022-12-15 10:32:14 -0800

    proj base

---
+++ proj/Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,20 @@
+public class HealthSystem {
+
+    private int health;
+
+    public HealthSystem(int health) {
+        this.health = health;
+    }
+
+    public int GetHealth() {
+        return health;
+    }
+
+    public void Damage(int damageAmount) {
+        health -= damageAmount;
+    }
+
+    public void Heal(int healAmount) {
+        health += healAmount;
+    }
+}
